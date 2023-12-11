5084c823753af64e2a32e15871b37f399bab02c3
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-12-11 12:19:54 +0100

    develop branch

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import TestScreen from "./src/screens/test-screen/TestScreen";
 
 export default function App() {
+  //add develop branch
   return (
     <View style={styles.container}>
       <TestScreen prop5={true} />
