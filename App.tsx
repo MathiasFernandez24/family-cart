454b1c7c760c96ce8ddb5c5ff728c7e500e174df
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-12-14 08:09:04 -0300

    create buttonComponent

--- App.tsx
+++ App.tsx
@@ -1,15 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import "./src/i18n";
 import TestScreen from "./src/screens/test-screen/TestScreen";
 import LanguageScreen from "./src/screens/language-screen/LanguageScreen";
+import { i18nTranslate } from "./src/i18n";
+import ButtonComponent from "./src/components/ButtonComponent";
 
 export default function App() {
+
+  const handlePress = () => {
+      console.log("boton presionado");
+      
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <TestScreen prop5={true} />
       <LanguageScreen />
+      <Text>{i18nTranslate("common.accept")}</Text>
+      
     </View>
   );
 }
@@ -21,4 +31,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  buttonContainer: {
+    borderWidth: 2,
+    borderColor: "red"
+  },
+  buttonText: {
+    fontSize: 25
+  },
+
 });
